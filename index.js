5ee66fe58c3cf82b679ffd840f0cf27175359512
jose cabreja <cabreja@yahoo.com>
2023-03-20 03:24:22 -0400

    /phase-0-pac-3-intro-to-functions-lab

---
+++ index.js
@@ -0,0 +1,24 @@
+function logShout(string){
+console.log(string.toUpperCase("hello"))}
+
+function logWhisper(string){
+    console.log(string.toLowerCase("HELLO"))}
+
+  
+function shout(string) {
+    return string.toUpperCase();
+  }
+
+  function whisper(string) {
+    return "hello".toLowerCase();
+  }
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string === 'hello')
+    return"I can't hear you!"
+    else if (string === 'HELLO')
+    return"YES INDEED!"
+    else if (string === "Let's have dinner together!") 
+    return"I would love to!"
+}
+ 
